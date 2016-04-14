bcf8f1876ecf9ef4bf9dd095cc8c1ca3be4e999d
mespla <mespla@dlsi.ua.es>
2016-04-14 08:22:02 +0200

    Fixing name of StringUtil

--- src-stubs/org/omegat/util/StringUtil.java
+++ src-stubs/org/omegat/util/StringUtil.java
@@ -21,6 +21,6 @@
  *************************************************************************/
 package org.omegat.util;
 
-public class StringUtils {
+public class StringUtil {
     public static String format(String str, Object... arguments) {return "";}
 }
